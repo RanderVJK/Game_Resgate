e901310cb47fb01f7702766e95872d0097fc5e8c
Vanderlei Kempf <dev.vjk@gmail.com>
2021-10-18 16:49:09 -0300

    Created the life bar

--- assets/js/js.js
+++ assets/js/js.js
@@ -7,6 +7,7 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 	$("#fundoGame").append("<div id='placar'></div>");
+	$("#fundoGame").append("<div id='energia'></div>");
 
 	//Principais variáveis do jogo
 	
@@ -15,6 +16,7 @@ function start() { // Inicio da função start()
 	var pontos=0;
 	var salvos=0;
 	var perdidos=0;
+	var energiaAtual=3;
 	var jogo = {};
 	var velocidade=5;
 	var posicaoY = parseInt(Math.random() * 334);
@@ -50,6 +52,7 @@ function start() { // Inicio da função start()
 	moveamigo();
 	colisao();
 	placar();
+	energia();
 	
 	} // Fim da função loop()
 
@@ -176,7 +179,8 @@ function start() { // Inicio da função start()
 		
 		// jogador com o inimigo1
 		if (colisao1.length>0) {
-			
+		
+		energiaAtual--;
 		inimigo1X = parseInt($("#inimigo1").css("left"));
 		inimigo1Y = parseInt($("#inimigo1").css("top"));
 		explosao1(inimigo1X,inimigo1Y);
@@ -189,6 +193,7 @@ function start() { // Inicio da função start()
 		// jogador com o inimigo2 
 		if (colisao2.length>0) {
 
+			energiaAtual--;
 			inimigo2X = parseInt($("#inimigo2").css("left"));
 			inimigo2Y = parseInt($("#inimigo2").css("top"));
 			explosao2(inimigo2X,inimigo2Y);
@@ -363,4 +368,31 @@ function start() { // Inicio da função start()
 		
 	} //fim da função placar()
 	
+	//Barra de energia
+
+	function energia() {
+	
+		if (energiaAtual==3) {
+			
+			$("#energia").css("background-image", "url(assets/imgs/energia3.png)");
+		}
+
+		if (energiaAtual==2) {
+			
+			$("#energia").css("background-image", "url(assets/imgs/energia2.png)");
+		}
+
+		if (energiaAtual==1) {
+			
+			$("#energia").css("background-image", "url(assets/imgs/energia1.png)");
+		}
+
+		if (energiaAtual==0) {
+			
+			$("#energia").css("background-image", "url(assets/imgs/energia0.png)");
+			
+			//Game Over
+			}
+	} // Fim da função energia()
+
 } // Fim da função start
